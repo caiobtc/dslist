825af0c0034a35e73e70b9e993fed921e349b9fb
Caio Henrique <henriquecaio898@gmail.com>
2024-11-13 11:14:50 -0300

    Game seed

--- src/main/java/com/caio/dslist/entities/Game.java
+++ src/main/java/com/caio/dslist/entities/Game.java
@@ -19,7 +19,11 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "Text")
     private String shortDescription;
+
+    @Column(columnDefinition = "Text")
     private String longDescription;
 
     public Game() {
